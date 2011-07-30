46d9560b5bb38026f765ba4069f551fb3c894d1a
Heng Li <lh3@live.co.uk>
2011-07-29 21:23:59 -0400

    remove the lookup table method...

--- rld.h
+++ rld.h
@@ -66,41 +66,13 @@ static inline void rld_dec_init(rld_t *e, uint64_t k)
 	e->r = 64;
 }
 
-static inline uint32_t rld_dec0a(rld_t *e)
-{
-	int y = 0, w;
-	uint64_t x;
-	x = e->p[0] << (64 - e->r) | (e->p < e->stail && e->r < 64? e->p[1] >> e->r : 0);
-	/* The following block does delta decoding. Code is duplicated here because this is faster. */
-	if (x >> 63 == 0) {
-		int z = rld_ddec_table[x >> 55];
-		if (z < 0) {
-			z = ~z, w = z & 0xf, y = z >> 4;
-		} else { // when z == 0, w = 0
-			int a = z>>4, b = z&0xf;
-			w = a + b, y = x << b >> (64 - a) | 1u << a;
-		}
-	} else w = y = 1;
-	if (w == 0) return 0;
-	y = y << e->abits | x << w >> (64 - e->abits);
-	w += e->abits;
-	if (e->r > w) e->r -= w;
-	else ++e->p, e->r = 64 + e->r - w;
-	return y;
-}
-
 static inline uint32_t rld_dec0(rld_t *e)
 {
 	int y = 0, w, l;
 	uint64_t x;
 	x = e->p[0] << (64 - e->r) | (e->p < e->stail && e->r < 64? e->p[1] >> e->r : 0);
 	if (x>>63 == 0) {
-		//w = 0x3333333355557790ll>>(x>>59<<2)&0xf;
-		if (x>>62 == 0) {
-			if (x>>61 == 0) {
-				if ((w = 0x55555555777799b0ll>>(x>>58<<2)&0xf) == 0) return 0;
-			} else w = 5;
-		} else w = 3;
+		if ((w = 0x333333335555779bll>>(x>>59<<2)&0xf) == 0xb && x>>58 == 0) return 0;
 		l = (x >> (64 - w)) - 1;
 		y = x << w >> (64 - l) | 1u << l;
 		w += l;
