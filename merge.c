714d68b31883b7fa5a360f70d31eaaad1d6b5d02
Heng Li <lh3@live.co.uk>
2011-08-10 11:07:38 -0400

    support long gaps; going to use B-tree

--- merge.c
+++ merge.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "fermi.h"
 #include "rld.h"
+#include "kvec.h"
 
 typedef struct {
 	rlditr_t itr;
@@ -9,20 +10,27 @@ typedef struct {
 	int64_t l;
 } rlditr2_t;
 
-uint32_t *fm_compute_gap0(const rld_t *e0, const rld_t *e1)
+typedef struct {
+	int32_t *gap;
+	kvec_t(int64_t) a;
+} gap0_t;
+
+#define GAP_MAX INT32_MAX
+
+gap0_t *fm_compute_gap0(const rld_t *e0, const rld_t *e1)
 {
-	uint32_t *gap;
+	gap0_t *g;
 	uint64_t k, l, *ok, *ol, i, j, x;
 	int c = 0;
+	g = calloc(1, sizeof(gap0_t));
 	ok = alloca(8 * e0->asize);
 	ol = alloca(8 * e0->asize);
-	gap = calloc(e0->mcnt[0], 4);
+	g->gap = calloc(e0->mcnt[0], 4);
 	x = e1->mcnt[1];
 	k = l = --x; // get the last sentinel of e1
 	j = i = e0->mcnt[1] - 1; // to modify gap[j]
-	++gap[j];
+	++g->gap[j];
 	for (;;) {
-		//printf("[%lld,%lld]@%d\n", k, l, c);
 		rld_rank2a(e1, k - 1, l, ok, ol);
 		for (c = 0; c < e1->asize; ++c)
 			if (ok[c] < ol[c]) break;
@@ -34,10 +42,15 @@ uint32_t *fm_compute_gap0(const rld_t *e0, const rld_t *e1)
 			j = e0->cnt[c] + rld_rank11(e0, i, c) - 1;
 			k = l = e1->cnt[c] + ok[c];
 		}
-		++gap[j];
+		if (g->gap[j] < 0) {
+			++g->a.a[-g->gap[j] - 1];
+		} else if (g->gap[j] == GAP_MAX) {
+			kv_push(int64_t, g->a, 1ll + GAP_MAX);
+			g->gap[j] = -(int64_t)g->a.n;
+		} else ++g->gap[j];
 		i = j;
 	}
-	return gap;
+	return g;
 }
 
 inline void rld_enc2(rld_t *e, rlditr2_t *itr2, int l, int c)
@@ -70,7 +83,7 @@ static inline void dec_enc(rld_t *e, rlditr2_t *itr, const rld_t *e0, rlditr_t *
 
 rld_t *fm_merge0(const rld_t *e0, const rld_t *e1)
 {
-	uint32_t *gap;
+	gap0_t *gap;
 	uint64_t i, k;
 	rlditr_t itr0, itr1;
 	rlditr2_t itr;
@@ -85,8 +98,8 @@ rld_t *fm_merge0(const rld_t *e0, const rld_t *e1)
 	rld_itr_init(e0, &itr0, 0);
 	rld_itr_init(e1, &itr1, 0);
 	for (i = k = 0; i < e0->mcnt[0]; ++i) {
-		uint64_t g = gap[i];
-		//printf("gap[%d]=%d\n", i, gap[i]);
+		int64_t g = gap->gap[i] < 0? gap->a.a[-gap->gap[i] - 1] : gap->gap[i];
+		//printf("gap[%d]=%lld\n", i, g);
 		if (g) {
 			//printf("%lld, %lld\n", g, k + 1);
 			dec_enc(e, &itr, e0, &itr0, &l0, &c0, k + 1);
@@ -98,6 +111,6 @@ rld_t *fm_merge0(const rld_t *e0, const rld_t *e1)
 	assert(l0 == 0 && l1 == 0); // both e0 and e1 stream should be finished
 	rld_enc(e, &itr.itr, itr.l, itr.c); // write the remaining symbols
 	rld_enc_finish(e, &itr.itr);
-	free(gap);
+	free(gap->gap); free(gap->a.a); free(gap);
 	return e;
 }
