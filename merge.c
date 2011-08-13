f4b493380c1813d6df1a196129dc5ba8930e2b93
Heng Li <lh3@live.co.uk>
2011-08-12 21:26:00 -0400

    simplify compute_gap_hash() as well

--- merge.c
+++ merge.c
@@ -170,31 +170,28 @@ rld_t *fm_merge_array(rld_t *e0, rld_t *e1, const char *fn)
 static gaphash_t *compute_gap_hash(const rld_t *e0, const rld_t *e1)
 {
 	gaphash_t *h;
-	uint64_t k, l, *ok, *ol, i, j, x;
+	uint64_t k, *ok, i, x;
 	int c = 0;
 	uint64_t n_processed = 1;
 	double t = cputime();
 	h = init_gaphash(e0->mcnt[0]);
 	ok = alloca(8 * e0->asize);
-	ol = alloca(8 * e0->asize);
 	x = e1->mcnt[1];
-	k = l = --x; // get the last sentinel of e1
-	j = i = e0->mcnt[1] - 1; // to modify gap[j]
-	insert_to_hash(h, j);
+	k = --x; // get the last sentinel of e1
+	i = e0->mcnt[1] - 1; // to modify gap[j]
+	insert_to_hash(h, i);
 	for (;;) {
-		rld_rank2a(e1, k - 1, l, ok, ol);
-		for (c = 0; c < e1->asize; ++c)
-			if (ok[c] < ol[c]) break;
+		c = rld_rank1a(e1, k, ok);
 		if (c == 0) {
-			j = e0->mcnt[1] - 1;
-			k = l = --x;
+			k = --x;
 			if (x == (uint64_t)-1) break;
+			i = e0->mcnt[1] - 1;
 		} else {
-			j = e0->cnt[c] + rld_rank11(e0, i, c) - 1;
-			k = l = e1->cnt[c] + ok[c];
+			k = e1->cnt[c] + ok[c] - 1;
+			rld_rank1a(e0, i, ok);
+			i = e0->cnt[c] + ok[c] - 1;
 		}
-		insert_to_hash(h, j);
-		i = j;
+		insert_to_hash(h, i);
 		if (++n_processed % MSG_SIZE == 0 && fm_verbose >= 3)
 			fprintf(stderr, "[M::%s] processed %lld million symbols in %.3f seconds (peak memory: %.3f MB).\n", __func__,
 					(long long)n_processed / 1000000, cputime() - t, rssmem());
