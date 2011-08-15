a05c625441eb19be86b264df5ddba01669364714
Heng Li <lh3@live.co.uk>
2011-08-15 12:37:57 -0400

    comment on the memory usage

--- utils.c
+++ utils.c
@@ -1,5 +1,7 @@
 #include <sys/resource.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <stdio.h>
 
 // 1: error; 2: warning; 3: message
 int fm_verbose = 3;
@@ -11,12 +13,48 @@ double cputime()
 	return r.ru_utime.tv_sec + r.ru_stime.tv_sec + 1e-6 * (r.ru_utime.tv_usec + r.ru_stime.tv_usec);
 }
 
+/* Comment on the memory usage. We may think getrusage() is the easiest way to
+ * get the memory usage of the current process.  However, on Linux, ru_maxrss
+ * is not supported by the kernel. We can get the current usage from file
+ * "/proc/self/stat" if the "/proc" file system is built in the kernel, but
+ * this is a snapshot instead of the maximum memory usage. We have to monitor
+ * the "stat" file constantly to get the maximum resident memory (RSS). On
+ * Linux, perhaps the easiest way to get the maximum RSS is to use my "runit".
+ * Even "/usr/bin/time" may not always work on all versions/distributions.
+ *
+ * On Mac, getrusage() writes ru_maxrss, but it seems to me that this field
+ * tends to be much larger than my expectation. This may be caused by the
+ * memory manager (i.e. malloc) instead of the kernel. When I have time,
+ * perhaps it is worth investigating an alternative memory manager or
+ * controling the major memory allocation by myself, which though could make
+ * the code look nasty.
+ */
+#if defined(__linux__)
+double rssmem()
+{
+	FILE *fp;
+	int c, n_spc = 0;
+	long mem, page_size;
+	page_size = sysconf(_SC_PAGESIZE);
+	fp = fopen("/proc/self/stat", "r");
+	while ((c = fgetc(fp)) != EOF) {
+		if (c == ' ') ++n_spc;
+		if (n_spc == 23) break;
+	}
+	fscanf(fp, "%ld", &mem);
+	fclose(fp);
+	return mem * page_size / 1024.0 / 1024.0;
+}
+#elif defined(__APPLE__)
 double rssmem()
 {
 	struct rusage r;
 	getrusage(RUSAGE_SELF, &r);
 	return r.ru_maxrss / 1024.0 / 1024.0;
 }
+#else
+double rssmem() { return 0.; }
+#endif
 
 double realtime()
 {
