4446418213176f17812b6f28f782af0317c6d695
Heng Li <lh3@live.co.uk>
2011-08-16 20:39:02 -0400

    multi-threaded writer

--- pmerge.c
+++ pmerge.c
@@ -5,12 +5,19 @@
 #include "utils.h"
 
 #define BLOCK_SIZE 0x1000000
+#define PWRITE_MIN 4
 
 typedef struct {
 	int step, n;
 	int64_t size, i, k, x, *buf;
 	const rld_t *e0, *e1;
 } worker_t;
+
+typedef struct {
+	int tid, n_threads;
+	worker_t *w;
+	uint64_t *bits;
+} writer_t;
 
 static void *worker(void *data)
 {
@@ -38,45 +45,73 @@ static void *worker(void *data)
 	return 0;
 }
 
+static void *writer(void *data)
+{
+	writer_t *w = (writer_t*)data;
+	int i, j;
+	for (j = 0; j < w->n_threads; ++j) {
+		worker_t *wo = w->w + j;
+		for (i = 0; i < wo->n; ++i) {
+			uint64_t x = wo->buf[i];
+			if ((x>>6) % w->n_threads == w->tid)
+				w->bits[x>>6] |= 1ull<<(x&0x3f);
+		}
+	}
+	return 0;
+}
+
 uint64_t *fm_compute_gap_bits(const rld_t *e0, const rld_t *e1, int n_threads)
 {
 	uint64_t *bits;
 	int64_t rest = e1->mcnt[0];
 	pthread_t *tid;
 	pthread_attr_t attr;
-	worker_t *data;
+	worker_t *wo;
+	writer_t *wr;
 	int j;
 	double tcpu, treal;
 
 	bits = xcalloc((e0->mcnt[0] + e1->mcnt[0] + 63) / 64, 8);
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	data = (worker_t*)calloc(n_threads, sizeof(worker_t));
+	wo = (worker_t*)calloc(n_threads, sizeof(worker_t));
+	wr = (writer_t*)calloc(n_threads, sizeof(writer_t));
 	tid = (pthread_t*)calloc(n_threads, sizeof(pthread_t));
 	for (j = 0; j < n_threads; ++j) {
-		worker_t *w = data + j;
+		worker_t *w = wo + j;
+		writer_t *r = wr + j;
 		w->e0 = e0; w->e1 = e1;
 		w->step = n_threads;
 		w->size = ((BLOCK_SIZE < rest? BLOCK_SIZE : rest) + n_threads - 1) / n_threads;
 		w->k = w->x = j;
 		w->i = w->e0->mcnt[1] - 1;
 		w->buf = malloc(w->size * 8);
+		r->n_threads = n_threads;
+		r->tid = j;
+		r->bits = bits;
+		r->w = wo;
 	}
 	tcpu = cputime(); treal = realtime();
 	while (rest) {
-		for (j = 0; j < n_threads; ++j) pthread_create(&tid[j], &attr, worker, data + j);
+		for (j = 0; j < n_threads; ++j) pthread_create(&tid[j], &attr, worker, wo + j);
 		for (j = 0; j < n_threads; ++j) pthread_join(tid[j], 0);
-		for (j = 0; j < n_threads; ++j) {
-			int i;
-			worker_t *w = data + j;
-			for (i = 0; i < w->n; ++i)
-				bits[w->buf[i]>>6] |= 1ull<<(w->buf[i]&0x3f);
-			rest -= w->n;
+		if (n_threads >= PWRITE_MIN) {
+			for (j = 0; j < n_threads; ++j) pthread_create(&tid[j], &attr, writer, wr + j);
+			for (j = 0; j < n_threads; ++j) pthread_join(tid[j], 0);
+			for (j = 0; j < n_threads; ++j) rest -= wo[j].n;
+		} else {
+			for (j = 0; j < n_threads; ++j) {
+				int i;
+				worker_t *w = wo + j;
+				for (i = 0; i < w->n; ++i)
+					bits[w->buf[i]>>6] |= 1ull<<(w->buf[i]&0x3f);
+				rest -= w->n;
+			}
 		}
 		if (fm_verbose >= 3)
 			fprintf(stderr, "[M::%s] %.3f million symbols remain; CPU time: %.3f; wall-clock / CPU: %.2f\n", __func__,
 					rest / 1e6, cputime()-tcpu, (cputime()-tcpu)/(realtime()-treal));
 	}
-	free(data); free(tid);
+	free(wo); free(wr); free(tid);
 	return bits;
 }
