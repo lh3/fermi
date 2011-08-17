83d953e7c8577faa7ec60c45b1129b972753d3a4
Heng Li <lh3@live.co.uk>
2011-08-16 21:24:58 -0400

    speed up the writer loop; to drop old bit impl.

--- pmerge.c
+++ pmerge.c
@@ -7,6 +7,10 @@
 #define BLOCK_SIZE 0x1000000
 #define PWRITE_MIN 4
 
+#ifndef kroundup32
+#define kroundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+#endif
+
 typedef struct {
 	int step, n;
 	int64_t size, i, k, x, *buf;
@@ -14,7 +18,7 @@ typedef struct {
 } worker_t;
 
 typedef struct {
-	int tid, n_threads;
+	int tid, n_workers, mask;
 	worker_t *w;
 	uint64_t *bits;
 } writer_t;
@@ -49,11 +53,11 @@ static void *writer(void *data)
 {
 	writer_t *w = (writer_t*)data;
 	int i, j;
-	for (j = 0; j < w->n_threads; ++j) {
+	for (j = 0; j < w->n_workers; ++j) {
 		worker_t *wo = w->w + j;
 		for (i = 0; i < wo->n; ++i) {
 			uint64_t x = wo->buf[i];
-			if ((x>>6) % w->n_threads == w->tid)
+			if ((x>>6&w->mask) == w->tid)
 				w->bits[x>>6] |= 1ull<<(x&0x3f);
 		}
 	}
@@ -68,25 +72,31 @@ uint64_t *fm_compute_gap_bits(const rld_t *e0, const rld_t *e1, int n_threads)
 	pthread_attr_t attr;
 	worker_t *wo;
 	writer_t *wr;
-	int j;
+	int j, n_writers;
 	double tcpu, treal;
 
 	bits = xcalloc((e0->mcnt[0] + e1->mcnt[0] + 63) / 64, 8);
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	wo = (worker_t*)calloc(n_threads, sizeof(worker_t));
-	wr = (writer_t*)calloc(n_threads, sizeof(writer_t));
 	tid = (pthread_t*)calloc(n_threads, sizeof(pthread_t));
 	for (j = 0; j < n_threads; ++j) {
 		worker_t *w = wo + j;
-		writer_t *r = wr + j;
 		w->e0 = e0; w->e1 = e1;
 		w->step = n_threads;
 		w->size = ((BLOCK_SIZE < rest? BLOCK_SIZE : rest) + n_threads - 1) / n_threads;
 		w->k = w->x = j;
 		w->i = w->e0->mcnt[1] - 1;
 		w->buf = malloc(w->size * 8);
-		r->n_threads = n_threads;
+	}
+	n_writers = n_threads;
+	kroundup32(n_writers);
+	if (n_writers > n_threads) n_writers >>= 1;
+	wr = (writer_t*)calloc(n_writers, sizeof(writer_t));
+	for (j = 0; j < n_writers; ++j) {
+		writer_t *r = wr + j;
+		r->n_workers = n_threads;
+		r->mask = n_writers - 1;
 		r->tid = j;
 		r->bits = bits;
 		r->w = wo;
@@ -96,8 +106,8 @@ uint64_t *fm_compute_gap_bits(const rld_t *e0, const rld_t *e1, int n_threads)
 		for (j = 0; j < n_threads; ++j) pthread_create(&tid[j], &attr, worker, wo + j);
 		for (j = 0; j < n_threads; ++j) pthread_join(tid[j], 0);
 		if (n_threads >= PWRITE_MIN) {
-			for (j = 0; j < n_threads; ++j) pthread_create(&tid[j], &attr, writer, wr + j);
-			for (j = 0; j < n_threads; ++j) pthread_join(tid[j], 0);
+			for (j = 0; j < n_writers; ++j) pthread_create(&tid[j], &attr, writer, wr + j);
+			for (j = 0; j < n_writers; ++j) pthread_join(tid[j], 0);
 			for (j = 0; j < n_threads; ++j) rest -= wo[j].n;
 		} else {
 			for (j = 0; j < n_threads; ++j) {
