278fcc7d9a25ccbf5c22f9049c6311f80c71d127
Heng Li <lh3@live.co.uk>
2011-08-22 10:17:35 -0400

    Merge pmerge.c to merge.c

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=			gcc
 CFLAGS=		-g -Wall -O2
 DFLAGS=		-D_DNA_ONLY #-D_USE_RLE6 #-DNDEBUG
-OBJS=		utils.o seq.o ksa.o ksa64.o saux.o rld.o exact.o merge.o pmerge.o \
+OBJS=		utils.o seq.o ksa.o ksa64.o saux.o rld.o exact.o merge.o \
 			append.o build.o cmd.o
 PROG=		fermi
 INCLUDES=	
@@ -24,7 +24,7 @@ rld.o:rld.h
 build.o:fermi.h rld.h
 append.o:fermi.h rld.h
 exact.o:fermi.h rld.h kstring.h kvec.h
-merge.o:fermi.h rld.h khash.h ksort.h
+merge.o:fermi.h rld.h ksort.h
 cmd.o:fermi.h rld.h kseq.h
 main.o:fermi.h
 
--- merge.c
+++ merge.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,7 +8,9 @@
 
 double cputime();
 
-uint64_t *fm_compute_gap_bits(const rld_t *e0, const rld_t *e1, int n_threads);
+/**********************************************
+ * Helpful routines for decoding and encoding *
+ **********************************************/
 
 typedef struct {
 	rlditr_t itr;
@@ -43,6 +46,98 @@ static inline void dec_enc(rld_t *e, rlditr2_t *itr, const rld_t *e0, rlditr_t *
 		*l0 = -k; *c0 = c;
 	}
 }
+
+/**************************
+ * Compute the bit vector *
+ **************************/
+
+#define BLOCK_SIZE 0x40000
+#define TIMER_INTV 64
+
+typedef struct {
+	int start, step;
+	const rld_t *e0, *e1;
+	uint64_t *bits;
+} worker_t;
+
+static inline void update_bits(int n, const int64_t *buf, uint64_t *bits)
+{
+	const int64_t *q, *end = buf + n;
+	for (q = buf; q != end; ++q) {
+		uint64_t *p = bits + (*q>>6);
+		uint64_t x = 1ull<<(*q&0x3f);
+		__sync_or_and_fetch(p, x); // SEE ALSO: http://gcc.gnu.org/onlinedocs/gcc-4.1.2/gcc/Atomic-Builtins.html
+	}
+}
+
+static void *worker(void *data)
+{
+	worker_t *w = (worker_t*)data;
+	int n = 0;
+	int64_t i, k, x, *buf, n_processed = 0;
+	uint64_t *ok;
+	double tcpu, treal;
+	tcpu = cputime(); treal = realtime();
+	ok = alloca(8 * w->e0->asize);
+	buf = malloc(BLOCK_SIZE * 8);
+	k = x = w->start;
+	i = w->e0->mcnt[1] - 1;
+	buf[n++] = i + k + 1;
+	for (;;) {
+		int c = rld_rank1a(w->e1, k, ok);
+		if (c == 0) {
+			x += w->step;
+			if (x >= w->e1->mcnt[1]) break;
+			k = x;
+			i = w->e0->mcnt[1] - 1;
+		} else {
+			k = w->e1->cnt[c] + ok[c] - 1;
+			rld_rank1a(w->e0, i, ok);
+			i = w->e0->cnt[c] + ok[c] - 1;
+		}
+		if (n == BLOCK_SIZE) {
+			update_bits(n, buf, w->bits);
+			if (fm_verbose >= 3 && ++n_processed % TIMER_INTV == 0)
+				fprintf(stderr, "[M::%s@%d] processed %.3f million symbols in %.3f / %.1f seconds.\n", __func__, w->start,
+						(double)n_processed*BLOCK_SIZE/1e6, cputime() - tcpu, (cputime() - tcpu) / (realtime() - treal));
+			n = 0;
+		}
+		buf[n++] = k + i + 1;
+	}
+	if (n) update_bits(n, buf, w->bits);
+	free(buf);
+	return 0;
+}
+
+uint64_t *fm_compute_gap_bits(const rld_t *e0, const rld_t *e1, int n_threads)
+{
+	uint64_t *bits;
+	pthread_t *tid;
+	pthread_attr_t attr;
+	worker_t *w;
+	int j;
+
+	bits = xcalloc((e0->mcnt[0] + e1->mcnt[0] + 63) / 64, 8);
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	w = (worker_t*)calloc(n_threads, sizeof(worker_t));
+	tid = (pthread_t*)calloc(n_threads, sizeof(pthread_t));
+	for (j = 0; j < n_threads; ++j) {
+		worker_t *ww = w + j;
+		ww->e0 = e0; ww->e1 = e1;
+		ww->step = n_threads;
+		ww->start = j;
+		ww->bits = bits;
+	}
+	for (j = 0; j < n_threads; ++j) pthread_create(&tid[j], &attr, worker, w + j);
+	for (j = 0; j < n_threads; ++j) pthread_join(tid[j], 0);
+	free(w); free(tid);
+	return bits;
+}
+
+/************************
+ * Merge two FM-indexes *
+ ************************/
 
 rld_t *fm_merge(rld_t *e0, rld_t *e1, int use_hash, int n_threads)
 {
@@ -87,6 +182,10 @@ rld_t *fm_merge(rld_t *e0, rld_t *e1, int use_hash, int n_threads)
 	rld_enc_finish(e, &itr.itr);
 	return e;
 }
+
+/**********************************
+ * Append a string to an FM-index *
+ **********************************/
 
 rld_t *fm_merge_from_SA(rld_t *e0, int len, const uint8_t *T, const int *SA, const int64_t *rank_l)
 {
