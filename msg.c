46fabba645a87b425e3c24bc082bb5b9fc39c4c7
Heng Li <lh3@live.co.uk>
2011-10-11 12:23:28 -0400

    refactor msg_print()

--- msg.c
+++ msg.c
@@ -2,33 +2,39 @@
 #include "rld.h"
 #include "kstring.h"
 
+void msg_write_node(const fmnode_t *p, long id, kstring_t *out)
+{
+	int j, k;
+	kputc('@', out); kputl(id, out);
+	for (j = 0; j < 2; ++j) {
+		kputc(' ', out);
+		kputl(p->k[j], out); kputc('>', out);
+		for (k = 0; k < p->nei[j].n; ++k) {
+			if (k) kputc(',', out);
+			kputl(p->nei[j].a[k].x, out); kputc(':', out);
+			kputw((int32_t)p->nei[j].a[k].y, out); kputc(':', out);
+			kputw((int32_t)(p->nei[j].a[k].y>>32), out);
+		}
+		if (p->nei[j].n == 0) kputc('.', out);
+	}
+	kputc('\n', out);
+	ks_resize(out, out->l + 2 * p->l + 5);
+	for (j = 0; j < p->l; ++j) out->s[out->l++] = "ACGT"[(int)p->seq[j] - 1];
+	out->s[out->l] = 0;
+	kputsn("\n+\n", 3, out);
+	kputsn(p->cov, p->l, out);
+	kputc('\n', out);
+}
+
 void msg_print(const fmnode_v *nodes)
 {
 	kstring_t out;
 	size_t i;
-	int j, k;
 	out.l = out.m = 0; out.s = 0;
 	for (i = 0; i < nodes->n; ++i) {
-		fmnode_t *p = &nodes->a[i];
 		out.l = 0;
-		kputc('@', &out); kputl(i, &out);
-		for (j = 0; j < 2; ++j) {
-			kputc(' ', &out);
-			kputl(p->k[j], &out); kputc('>', &out);
-			for (k = 0; k < p->nei[j].n; ++k) {
-				if (k) kputc(',', &out);
-				kputl(p->nei[j].a[k].x, &out); kputc(':', &out);
-				kputw((int32_t)p->nei[j].a[k].y, &out); kputc(':', &out);
-				kputw((int32_t)(p->nei[j].a[k].y>>32), &out);
-			}
-			if (p->nei[j].n == 0) kputc('.', &out);
-		}
-		kputc('\n', &out);
-		ks_resize(&out, out.l + p->l + 3);
-		for (j = 0; j < p->l; ++j) out.s[out.l++] = "ACGT"[(int)p->seq[j] - 1];
-		kputs("\n+", &out);
-		out.s[out.l] = 0;
-		puts(out.s);
-		puts(p->cov);
+		msg_write_node(&nodes->a[i], i, &out);
+		fputs(out.s, stdout);
 	}
+	free(out.s);
 }
