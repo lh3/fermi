bb5e12efb0e3f03787cb4aade328126c35f74da5
Heng Li <lh3@me.com>
2012-04-18 11:20:05 -0400

    output mapping quality

--- misc/sam2iden.d
+++ misc/sam2iden.d
@@ -6,7 +6,7 @@ void main(string[] args)
 {
 	if (args.length == 1) {
 		writeln("\nUsage: sam2iden.d <in.sam>\n");
-		writeln("Output format: queryName, queryStart, queryEnd, strand, refStart, refEnd, blastIdentity, blatIdentity\n");
+		writeln("Output format: queryName, queryStart, queryEnd, strand, refStart, refEnd, mapQ, blastIdentity, blatIdentity\n");
 		return;
 	}
 	auto fp = new ZFile(args[1]);
@@ -27,7 +27,7 @@ void main(string[] args)
 		if (flag&16) write(cs.clip[1], '\t', qlen - cs.clip[0], "\t-\t");
 		else write(cs.clip[0], '\t', qlen - cs.clip[1], "\t+\t");
 		qlen -= cs.clip[0] + cs.clip[1]; // qlen is now the alignment length
-		write(t[2], '\t', pos, '\t', pos + cs.n_M + cs.n_D, '\t',
+		write(t[2], '\t', pos, '\t', pos + cs.n_M + cs.n_D, '\t', t[4], '\t',
 			  cast(double)(qlen + cs.n_D - ndiff) / (qlen + cs.n_D), '\t',
 			  cast(double)(qlen - cs.n_I - (ndiff - cs.n_I - cs.n_D)) / (qlen - cs.n_I), '\n');
 	}
