de31ef7c5cbf911e750f77dc3929635e6ed82531
Heng Li <lh3@me.com>
2013-05-07 09:09:31 -0400

    r763: ropebwt output RLD format
    also simplified the CLI for short reads, using a preset.

--- ropebwt.c
+++ ropebwt.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "bprope6.h"
 #include "bcr.h"
+#include "rld.h"
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
 
@@ -18,6 +19,7 @@ enum algo_e { BPR, RBR, BCR };
 #define FLAG_TREE 0x10
 #define FLAG_CUTN 0x20
 #define FLAG_DROPN 0x40
+#define FLAG_RLD 0x80
 
 static void insert1(int flag, int l, uint8_t *s, bprope6_t *bpr, bcr_t *bcr)
 {
@@ -50,18 +52,18 @@ int main_ropebwt(int argc, char *argv[])
 	bcr_t *bcr = 0;
 	gzFile fp;
 	FILE *out = stdout;
-	char *tmpfn = 0;
+	char *tmpfn = 0, *outfn = 0;
 	kseq_t *ks;
 	enum algo_e algo = BPR;
-	int c, max_runs = 512, max_nodes = 64, min_len = 1;
+	int c, max_runs = 512, max_nodes = 64, min_len = 1, is_short = 0;
 	int bcr_flag = 0, flag = FLAG_FOR | FLAG_REV | FLAG_ODD;
 
-	while ((c = getopt(argc, argv, "TFRODbNo:r:n:ta:f:v:sBl:")) >= 0)
+	while ((c = getopt(argc, argv, "TFRODbNo:r:n:ta:f:v:sBl:dS")) >= 0)
 		if (c == 'a') {
 			if (strcmp(optarg, "bpr") == 0) algo = BPR;
 			else if (strcmp(optarg, "bcr") == 0) algo = BCR;
 			else fprintf(stderr, "[W::%s] available algorithms: bpr or bcr; default to bpr\n", __func__);
-		} else if (c == 'o') out = fopen(optarg, "wb");
+		} else if (c == 'o') out = fopen(optarg, "wb"), outfn = optarg;
 		else if (c == 'F') flag &= ~FLAG_FOR;
 		else if (c == 'R') flag &= ~FLAG_REV;
 		else if (c == 'O') flag &= ~FLAG_ODD;
@@ -69,6 +71,7 @@ int main_ropebwt(int argc, char *argv[])
 		else if (c == 'b') flag |= FLAG_BIN;
 		else if (c == 'N') flag |= FLAG_CUTN;
 		else if (c == 'D') flag |= FLAG_DROPN;
+		else if (c == 'd') flag |= FLAG_RLD;
 		else if (c == 't') bcr_flag |= BCR_F_THR;
 		else if (c == 's') bcr_flag |= BCR_F_RLO;
 		else if (c == 'B') bcr_flag |= BCR_F_BPR;
@@ -77,11 +80,20 @@ int main_ropebwt(int argc, char *argv[])
 		else if (c == 'n') max_nodes= atoi(optarg);
 		else if (c == 'f') tmpfn = optarg;
 		else if (c == 'v') bcr_verbose = atoi(optarg);
+		else if (c == 'S') is_short = 1;
+	
+	if (is_short) {
+		algo = BCR;
+		flag = FLAG_FOR | FLAG_REV | FLAG_ODD | FLAG_RLD | FLAG_CUTN;
+		min_len = 25;
+		bcr_verbose = 3;
+	}
 
 	if (optind == argc) {
 		fprintf(stderr, "\n");
 		fprintf(stderr, "Usage:   ropebwt [options] <in.fq.gz>\n\n");
 		fprintf(stderr, "Options: -a STR     algorithm: bpr or bcr [bpr]\n");
+		fprintf(stderr, "         -S         build FM-index for short sequences (-Ndstabcr -l25 -v3)\n");
 		fprintf(stderr, "         -r INT     max number of runs in leaves (bpr only) [%d]\n", max_runs);
 		fprintf(stderr, "         -n INT     max number children per internal node (bpr only) [%d]\n", max_nodes);
 		fprintf(stderr, "         -o FILE    output file [stdout]\n");
@@ -89,6 +101,7 @@ int main_ropebwt(int argc, char *argv[])
 		fprintf(stderr, "         -v INT     verbose level (bcr only) [%d]\n", bcr_verbose);
 		fprintf(stderr, "         -l INT     skip sequences/fragments shorter than INT bp [%d]\n", min_len);
 		fprintf(stderr, "         -b         binary output (5+3 runs starting after 4 bytes)\n");
+		fprintf(stderr, "         -d         fermi's RLD output\n");
 		fprintf(stderr, "         -s         sort reads into RLO order (bcr only)\n");
 		fprintf(stderr, "         -B         use rope to keep partial BWT (bcr only)\n");
 		fprintf(stderr, "         -t         enable threading (bcr only)\n");
@@ -136,12 +149,23 @@ int main_ropebwt(int argc, char *argv[])
 	kseq_destroy(ks);
 	gzclose(fp);
 
-#define print_bwt(itr_t, itr_set, itr_next_f, is_bin, fp) do { \
+#define print_bwt(itr_t, itr_set, itr_next_f, is_rld, is_bin, fp) do { \
 		itr_t *itr; \
 		const uint8_t *s; \
 		int i, j, l; \
 		itr = (itr_set); \
-		if (is_bin) { \
+		if (is_rld) { \
+			rld_t *e; \
+			rlditr_t ri; \
+			e = rld_init(6, 3); \
+			rld_itr_init(e, &ri, 0); \
+			while ((s = itr_next_f(itr, &l)) != 0) \
+				for (i = 0; i < l; ++i) \
+					rld_enc(e, &ri, s[i]>>3, s[i]&7); \
+			rld_enc_finish(e, &ri); \
+			rld_dump(e, outfn? outfn : "-"); \
+			rld_destroy(e); \
+		} else if (is_bin) { \
 			fwrite("RLE\6", 4, 1, fp); \
 			while ((s = itr_next_f(itr, &l)) != 0) \
 				fwrite(s, 1, l, fp); \
@@ -156,13 +180,13 @@ int main_ropebwt(int argc, char *argv[])
 	} while (0)
 
 	if (bpr) {
-		print_bwt(bpriter_t, bpr_iter_init(bpr), bpr_iter_next, flag&FLAG_BIN, out);
+		print_bwt(bpriter_t, bpr_iter_init(bpr), bpr_iter_next, flag&FLAG_RLD, flag&FLAG_BIN, out);
 		if (flag&FLAG_TREE) bpr_print(bpr);
 		bpr_destroy(bpr);
 	}
 	if (bcr) {
 		bcr_build(bcr, bcr_flag, tmpfn);
-		print_bwt(bcritr_t, bcr_itr_init(bcr), bcr_itr_next, flag&FLAG_BIN, out);
+		print_bwt(bcritr_t, bcr_itr_init(bcr), bcr_itr_next, flag&FLAG_RLD, flag&FLAG_BIN, out);
 		bcr_destroy(bcr);
 	}
 	fclose(out);
